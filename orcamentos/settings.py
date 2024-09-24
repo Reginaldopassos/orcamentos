a1480c4262d979592d9a5b5383c349ccf95ba3e2
Reginaldopassos <reginaldopassos16@gmail.com>
2024-09-24 01:04:52 -0300

    Enviando arquivos para deploy

--- orcamentos/settings.py
+++ orcamentos/settings.py
@@ -129,13 +129,13 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-'''
+
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-'''
 
+"""
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
@@ -145,7 +145,7 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR / "media"
-
+""""""
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
